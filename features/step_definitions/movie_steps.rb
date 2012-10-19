29a5b3924b80422f39110d1864954bc15c6af6c9
saasbook <saasbook@saasbook.(none)>
2012-10-19 09:13:01 -0700

    Part 1 is passing

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    m = Movie.new
+    m.title = movie["title"]
+    m.rating = movie["rating"]
+    m.release_date = movie["release_date"]
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
